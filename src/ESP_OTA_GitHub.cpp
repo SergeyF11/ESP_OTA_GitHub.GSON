e5fb2ca8ac9d63f82f934b63d1f0f6ad830c4b55
Sergey <sergey.fedotov@smilart.com>
2024-12-24 19:37:58 +0300

    Перевел на библиотеку GSON. Ускорил проверку обновлений

--- src/ESP_OTA_GitHub.cpp
+++ src/ESP_OTA_GitHub.cpp
@@ -16,22 +16,36 @@ ESPOTAGitHub::ESPOTAGitHub(BearSSL::CertStore* certStore, const char* user, cons
     _currentTag = currentTag;
     _binFile = binFile;
     _preRelease = preRelease;
-    _lastError = "";
-    _upgradeURL = "";
+    _lastError = NULL_STR;
+    _upgradeURL = NULL_STR;
+    _releaseTag = NULL_STR;
+}
+ESPOTAGitHub::ESPOTAGitHub( const char* user, const char* repo, const char* currentTag, const char* binFile, bool preRelease) {
+    _certStore = nullptr;
+    _user = user;
+    _repo = repo;
+    _currentTag = currentTag;
+    _binFile = binFile;
+    _preRelease = preRelease;
+    _lastError = NULL_STR;
+    _upgradeURL = NULL_STR;
+    _releaseTag = NULL_STR;
 }
 
-/* Private methods */
+static const char _http[] PROGMEM = "http://";
+static const char _https[] PROGMEM = "https://";
 
+/* Private methods */
 urlDetails_t ESPOTAGitHub::_urlDetails(String url) {
     String proto = "";
     int port = 80;
-    if (url.startsWith("http://")) {
-        proto = "http://";
-        url.replace("http://", "");
+    if (url.startsWith( _http )){ //"http://")) {
+        proto = _http; //"http://";
+        url.replace( _http /*"http://"*/, "");
     } else {
-        proto = "https://";
+        proto = _https; //"https://";
         port = 443;
-        url.replace("https://", "");
+        url.replace(_https /*"https://"*/, "");
     }
     int firstSlash = url.indexOf('/');
     String host = url.substring(0, firstSlash);
@@ -45,7 +59,31 @@ urlDetails_t ESPOTAGitHub::_urlDetails(String url) {
     urlDetail.path = path;
 
     return urlDetail;
-}
+};
+
+urlDetails_t ESPOTAGitHub::_urlDetails(urlDetails_t& urlDetail, String url) {
+    //String proto = "";
+    //int port = 80;
+    if (url.startsWith( _http )){ //"http://")) {
+        urlDetail.proto = _http; //"http://";
+        urlDetail.port = 80;
+        url.replace( _http /*"http://"*/, "");
+    } else {
+        urlDetail.proto = _https; //"https://";
+        urlDetail.port = 443;
+        url.replace(_https /*"https://"*/, "");
+    }
+    int firstSlash = url.indexOf('/');
+    // String host = url.substring(0, firstSlash);
+    // String path = url.substring(firstSlash);
+
+    //urlDetail.proto = proto;
+    urlDetail.host = url.substring(0, firstSlash);
+    //urlDetail.port = port;
+    urlDetail.path = url.substring(firstSlash);;
+
+    return urlDetail;
+};
 
 bool ESPOTAGitHub::_resolveRedirects() {
     urlDetails_t splitURL = _urlDetails(_upgradeURL);
@@ -56,32 +94,52 @@ bool ESPOTAGitHub::_resolveRedirects() {
     bool isFinalURL = false;
 
     BearSSL::WiFiClientSecure client;
-    client.setCertStore(_certStore);
+    if ( _certStore == nullptr ){
+        client.setInsecure();
+    } else {
+        client.setCertStore(_certStore);
+    }
 
     while (!isFinalURL) {
         if (!client.connect(host, port)) {
-            _lastError = "Connection Failed.";
+            _lastError = F("Connection Failed.");
             return false;
         }
 
-        client.print(String("GET ") + path + " HTTP/1.1\r\n" +
-            "Host: " + host + "\r\n" +
-            "User-Agent: ESP_OTA_GitHubArduinoLibrary\r\n" +
-            "Connection: close\r\n\r\n");
+ 
+    client.print(F("GET "));
+    client.print(path);
+    client.print(F(" HTTP/1.1\r\n"));
+    client.print(F("Host: ") ); client.print( host );
+    client.print(F("\r\n"));
+    client.print(F("User-Agent: ESP_OTA_GitHubArduinoLibrary\r\n"));
+    client.print(F("Connection: close\r\n\r\n"));
+
+        // client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+        //     "Host: " + host + "\r\n" +
+        //     "User-Agent: ESP_OTA_GitHubArduinoLibrary\r\n" +
+        //     "Connection: close\r\n\r\n");
 
         while (client.connected()) {
             String response = client.readStringUntil('\n');
-            if (response.startsWith("location: ") || response.startsWith("Location: ")) {
-                isFinalURL = false;
-                String location = response;
-				if (response.startsWith("location: ")) {
-					location.replace("location: ", "");
-				} else {
-					location.replace("Location: ", "");
-				}
-                location.remove(location.length() - 1);
+            // Serial.print("Resp:");
+            // Serial.println( response);
+            // Serial.println( response.substring(0,9));
 
-                if (location.startsWith("http://") || location.startsWith("https://")) {
+            if ( response.substring(0,9).equalsIgnoreCase( F("location:")) ){
+            //if (response.startsWith("location: ") || response.startsWith("Location: ")) {
+                isFinalURL = false;
+                String location = response.substring(10, response.length() -1 );
+                
+				// if (response.startsWith("location: ")) {
+				// 	location.replace("location: ", "");
+				// } else {
+				// 	location.replace("Location: ", "");
+				// }
+                //location.remove(location.length() - 1);
+                //Serial.printf("Location: %s\n", location.c_str());
+
+                if (location.startsWith(_http) || location.startsWith(_https)) {
                     //absolute URL - separate host from path
                     urlDetails_t url = _urlDetails(location);
                     proto = url.proto;
@@ -102,29 +160,35 @@ bool ESPOTAGitHub::_resolveRedirects() {
                 break;
             }
         }
+        
     }
 
     if(isFinalURL) {
         String finalURL = proto + host + path;
         _upgradeURL = finalURL;
+        //Serial.printf("Final URL=%s\n", finalURL.c_str() );
+        
         return true;
     } else {
-        _lastError = "CONNECTION FAILED";
+        _lastError = F("CONNECTION FAILED");
         return false;
     }
 }
 
 // Set time via NTP, as required for x.509 validation
 void ESPOTAGitHub::_setClock() {
-	configTime(0, 0, GHOTA_NTP1, GHOTA_NTP2);  // UTC
+    if ( _certStore == nullptr || time(nullptr) > 8 *3600 *2 ) return;
+    //Serial.println(__PRETTY_FUNCTION__);
+	configTime("GMT-0", GHOTA_NTP1, GHOTA_NTP2);  // UTC
 
 	time_t now = time(nullptr);
 	while (now < 8 * 3600 * 2) {
 		yield();
+        Serial.print(".");
 		delay(500);
 		now = time(nullptr);
 	}
-
+    //Serial.println(" done");
 	struct tm timeinfo;
 	gmtime_r(&now, &timeinfo);
 }
@@ -133,25 +197,50 @@ void ESPOTAGitHub::_setClock() {
 
 bool ESPOTAGitHub::checkUpgrade() {
 	
-	_setClock(); // Clock needs to be set to perform certificate checks
-	
     BearSSL::WiFiClientSecure client;
-    client.setCertStore(_certStore);
+    if ( _certStore == nullptr ){
+        client.setInsecure();
+//        Serial.print("Insecure git client");
+    }else {
+        _setClock(); // Clock needs to be set to perform certificate checks
+        client.setCertStore(_certStore);
+//        Serial.print("Secure git client");
+    }
     if (!client.connect(GHOTA_HOST, GHOTA_PORT)) {
         _lastError = "Connection failed";
         return false;
     }
-	  
-    String url = "/repos/";
-    url += _user;
-    url += "/";
-    url += _repo;
-    url += "/releases/latest";
-	  
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+	//Serial.print("Connect "GHOTA_HOST":"); Serial.println(GHOTA_PORT);  
+    // String url = "/repos/";
+    // url += _user;
+    // url += "/";
+    // url += _repo;
+    // url += "/releases/latest";
+
+    //Serial.println("Send GET request");  
+    
+    // client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+    //     "Host: " + GHOTA_HOST + "\r\n" +
+    //     "User-Agent: ESP_OTA_GitHubArduinoLibrary\r\n" +
+    //     "Connection: close\r\n\r\n");
+    client.print(F("GET /repos/"));
+    //client.print(url);
+    client.print( _user );
+    client.print('/');
+    client.print( _repo);
+    client.print(F("/releases/latest"));
+    
+    client.print(F(" HTTP/1.1\r\n"));
+    client.print(F("Host: ") ); client.print( GHOTA_HOST );
+    client.print(F("\r\n"));
+    client.print(F("User-Agent: ESP_OTA_GitHubArduinoLibrary\r\n"));
+    client.print(F("Connection: close\r\n\r\n"));
+    
+/*     Serial.println( String("GET ") + url + " HTTP/1.1\r\n" +
         "Host: " + GHOTA_HOST + "\r\n" +
         "User-Agent: ESP_OTA_GitHubArduinoLibrary\r\n" +
-        "Connection: close\r\n\r\n");
+        "Connection: close\r\n\r\n"); */
+//    Serial.println("sended. Wait response...");
 
     while (client.connected()) {
         String response = client.readStringUntil('\n');
@@ -159,68 +248,80 @@ bool ESPOTAGitHub::checkUpgrade() {
 			break;
         }
     }
-    
-	String response = client.readStringUntil('\n');
-    //client->stop();
-	  
-	// --- ArduinoJSON v6 --- //
-	 
-	// Get from https://arduinojson.org/v6/assistant/
-	const size_t capacity = JSON_ARRAY_SIZE(3) + 3*JSON_OBJECT_SIZE(13) + 5*JSON_OBJECT_SIZE(18) + 5560;
 
-	DynamicJsonDocument doc(capacity);
-	  
-	DeserializationError error = deserializeJson(doc, response);
-	  
-	if (!error) {
-		if (doc.containsKey("tag_name")) {
-			const char* release_tag = doc["tag_name"];
-            const char* release_name = doc["name"];
-            bool release_prerelease = doc["prerelease"];
-			if (strcmp(release_tag, _currentTag) != 0) {
+
+    client.setTimeout(GHOTA_TIMEOUT);
+    String response = client.readString();
+    gson::Parser doc;
+    while ( client.available() || ! doc.parse(response.c_str(), response.length())){
+        delay(10);
+        Serial.println(response);
+        response += client.readString();
+    }
+
+		if ( doc.has("tag_name")) {
+            //Serial.print("tag name found: ");
+			_releaseTag = doc["tag_name"].toString();
+            String release_name = doc["name"].toString();
+            bool release_prerelease = doc["prerelease"].toBool();
+            //bool release_prerelease = doc["Pre_Beta_release"];
+//Serial.println(_releaseTag );
+           
+			if (strcmp(_releaseTag.c_str(), _currentTag) != 0) {
 				if (!_preRelease) {
 					if (release_prerelease) {
-						_lastError = "Latest release is a pre-release and GHOTA_ACCEPT_PRERELEASE is set to false.";
+						_lastError = F("Latest release is a pre-release and GHOTA_ACCEPT_PRERELEASE is set to false.");
 						return false;
 					}
 				}
-				JsonArray assets = doc["assets"];
-				bool valid_asset = false;
-				for (auto asset : assets) {
-					const char* asset_type = asset["content_type"];
-					const char* asset_name = asset["name"];
-					const char* asset_url = asset["browser_download_url"];
+
+                if ( doc.has("assets") && doc["assets"].isArray() ){ //&& doc["assets"].isArray() ){
+// Serial.print("Assets:");  doc["assets"].stringify(Serial); Serial.println();
+                int i = 0;
+                bool valid_asset = false;
+                
+                while(  doc["assets"][i].isObject() ){
+                
+                //Serial.printf("Asset[%d]:%s\n", i, doc["assets"][i]["name"].toString().c_str()); 
+                //doc["assets"][i].stringify(Serial);
+                //Serial.println();
+
+					String asset_type = doc["assets"][i]["content_type"].toString();
+					String asset_name = doc["assets"][i]["name"].toString();
+					String asset_url = doc["assets"][i]["browser_download_url"].toString();
 					  
-					if (strcmp(asset_type, GHOTA_CONTENT_TYPE) == 0 && strcmp(asset_name, _binFile) == 0) {
+					if (strcmp(asset_type.c_str(), GHOTA_CONTENT_TYPE) == 0 && strcmp(asset_name.c_str(), _binFile) == 0) {
 						_upgradeURL = asset_url;
 						valid_asset = true;
+                        break;
 					} else {
 						valid_asset = false;
 					}
-				}
+                    i++;
+              	}
+                doc.reset();
 				if (valid_asset) {
+                    //Serial.println("Parsing done");
 					return true;
 				} else {
-					_lastError = "No valid binary found for latest release.";
+					_lastError = F("No valid binary found for latest release.");
 					return false;
 				}
 			} else {
-				_lastError = "Already running latest release.";
+				_lastError = F("Already running latest release.");
                 return false;
 			}
 		} else {
-			_lastError = "JSON didn't match expected structure. 'tag_name' missing.";
+			_lastError = F("JSON didn't match expected structure. 'tag_name' missing.");
             return false;
 		}
 	} else {
-		_lastError = "Failed to parse JSON."; // Error was: " + error.c_str();
+		_lastError = F("Failed to parse JSON."); // Error was: " + error.c_str();
         return false;
 	}
-	// --- END ArduinoJSON v6 --- //
 }
-
 bool ESPOTAGitHub::doUpgrade() {
-    if (_upgradeURL == "") {
+    if (_upgradeURL.isEmpty()) {
         //_lastError = "No upgrade URL set, run checkUpgrade() first.";
         //return false;
 		
@@ -234,16 +335,20 @@ bool ESPOTAGitHub::doUpgrade() {
 
 	_resolveRedirects();
 	
-    urlDetails_t splitURL = _urlDetails(_upgradeURL);
+    urlDetails_t splitURL= _urlDetails(_upgradeURL);
 	
     BearSSL::WiFiClientSecure client;
     bool mfln = client.probeMaxFragmentLength(splitURL.host, splitURL.port, 1024);
     if (mfln) {
         client.setBufferSizes(1024, 1024);
     }
-    client.setCertStore(_certStore);
-
+    if ( _certStore == nullptr ){
+        client.setInsecure();
+    }else {
+        client.setCertStore(_certStore);
+    }
     ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+    ESPhttpUpdate.rebootOnUpdate(false);
     t_httpUpdate_return ret = ESPhttpUpdate.update(client, _upgradeURL);
 
     switch (ret) {
@@ -252,13 +357,19 @@ bool ESPOTAGitHub::doUpgrade() {
             return false;
 
         case HTTP_UPDATE_NO_UPDATES:
-            _lastError = "HTTP_UPDATE_NO_UPDATES";
+            _lastError = F("HTTP_UPDATE_NO_UPDATES");
             return false;
 
         case HTTP_UPDATE_OK:
-            _lastError = "HTTP_UPDATE_OK";
+            _lastError = F("HTTP_UPDATE_OK");
             return true;
     }
+    return false;
+}
+
+bool ESPOTAGitHub::doUpgrade(String& url) {
+    _upgradeURL = url;
+    return this->doUpgrade();
 }
 
 String ESPOTAGitHub::getLastError() {
@@ -267,4 +378,8 @@ String ESPOTAGitHub::getLastError() {
 
 String ESPOTAGitHub::getUpgradeURL() {
     return _upgradeURL;
+}
+
+String ESPOTAGitHub::getLatestTag() {
+    return _releaseTag;
 }
--- src/ESP_OTA_GitHub.h
+++ src/ESP_OTA_GitHub.h
@@ -12,39 +12,48 @@
 
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
-#include <ArduinoJson.h>
-#include <time.h>
+//#include <ArduinoJson.h>
+//#include <time.h>
+#include <GSON.h>
 
 #define GHOTA_HOST "api.github.com"
 #define GHOTA_PORT 443
-#define GHOTA_TIMEOUT 1500
+#define GHOTA_TIMEOUT 500
 #define GHOTA_CONTENT_TYPE "application/octet-stream"
 
-#define GHOTA_NTP1 "pool.ntp.org"
-#define GHOTA_NTP2 "time.nist.gov"
+#define GHOTA_NTP1 "ntp1.stratum2.ru"
+#define GHOTA_NTP2 "ru.pool.ntp.org"
+#define NULL_STR (char*)nullptr
 
-typedef struct urlDetails_t {
+struct urlDetails_t {
     String proto;
     String host;
 	int port;
     String path;
 };
 
+
 class ESPOTAGitHub {
 	public:
 		ESPOTAGitHub(BearSSL::CertStore* certStore, const char* user, const char* repo, const char* currentTag, const char* binFile, bool preRelease);
+		ESPOTAGitHub(const char* user, const char* repo, const char* currentTag, const char* binFile, bool preRelease);
 		bool checkUpgrade();
 		bool doUpgrade();
+		bool doUpgrade(String&);
 		String getLastError();
 		String getUpgradeURL();
+		String getLatestTag();
 	private:
 		void _setClock(); //Set time via NTP, as required for x.509 validation
 		urlDetails_t _urlDetails(String url); // Separates a URL into protocol, host and path into a custom struct
+		urlDetails_t _urlDetails(urlDetails_t& splitDetail, String url); // Separates a URL into protocol, host and path into a custom struct
+
 		bool _resolveRedirects(); // Follows re-direct sequences until a "real" url is found.
 		
 		BearSSL::CertStore* _certStore;
 		String _lastError; // Holds the last error generated
-		String _upgradeURL; // Holds the upgrade URL (changes when _resolveRedirects() is run).
+		String _upgradeURL; // Holds the upgrade URL (changes when getFinalURL() is run).
+		String _releaseTag;
 		const char* _user;
 		const char* _repo;
 		const char* _currentTag;
